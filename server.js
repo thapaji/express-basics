323c713fb06375c5a46c599beb406e45286fe838
Sujan <mail.thapasujan@gmail.com>
2024-04-15 11:49:01 +1000

    Post and get method for server side rendering done

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
-app.get("/resister",(req,res) =>{
-    res.sendFile(__dirname+'/public/resister.html');
-})
+app.get("/resister", (req, res) => {
+  res.sendFile(__dirname + "/public/resister.html");
+});
 
 app.get("/", (req, res) => {
   console.log("sercer received the request");
 
-//   res.send(`<h1>Home Page</h1>
-//     <a href="/resister"><button>Resister</button></a>
-//     <a href="/login"><button>Login</button></a>`);
+  //   res.send(`<h1>Home Page</h1>
+  //     <a href="/resister"><button>Resister</button></a>
+  //     <a href="/login"><button>Login</button></a>`);
 
-    res.sendFile(__dirname+'/public/index.html');
+  res.sendFile(__dirname + "/public/index.html");
+});
 
+app.use(express.urlencoded())
+app.post("/resister", (req, res) => {
+  console.log(req.body);
+  res.send('Data Received')
 });
 
 app.listen(8000, (error) => {
